44668851ce831ed5cf39bee7b1293f90977c0d81
Dima Panasyuk <dima_panasyuk@ukr.net>
2016-10-27 23:59:19 +0300

    Fixed bug with serving files

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT || 3000;
 const rootDir = path.resolve(__dirname, '.');
 let app = express();
 
+app.use(express.static(path.resolve(rootDir, 'public', 'build')));
 app.get('*', (req, res, next) => {
-  res.send(path.resolve(rootDir, 'public', 'build', 'index.html'));
+  res.sendFile(path.resolve(rootDir, 'public', 'build', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log(`App is runnning on port ${3000}!`);
+app.listen(port, () => {
+  console.log(`App is runnning on port ${port}!`);
 });
